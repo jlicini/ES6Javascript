05280988648aa709ce789493ed4faf52229164a6
jlicini <licinijenny@gmail.com>
2021-11-18 12:19:43 +0100

    const and let

--- variable.js
+++ variable.js
@@ -15,3 +15,40 @@ title = 'Senior Software Engineer'
 hourlyWage = 60;
 
 console.log(nameEmp, title, hourlyWage)
+
+console.log("---")
+
+function count(targetString) {
+    const characters = ['a', 'e', 'i', 'o', 'u'];
+    let number = 0
+
+    for (let i = 0; i < targetString.length; i++) {
+        if(characters.includes(targetString[i])) number++ 
+    }
+
+    return number
+}
+
+count('shfsjhbfsfs')
+
+console.log("---")
+
+const name = 'Anna';
+let age = 24;
+const dateOfBirth = '24/07/1997';
+
+console.log("---")
+
+const statuses = [ 
+    { code: 'OK', response: 'Request successful' },
+    { code: 'FAILED', response: 'There was an error with your request' },
+    { code: 'PENDING', response: 'Your reqeust is still pending' }
+  ];
+  let message = '';
+  const currentCode = 'OK';
+
+  for (var i = 0; i < statuses.length; i++) {
+    if (statuses[i].code === currentCode) {
+      message = statuses[i].response;
+    }
+  }
