12a5c1d0aee1f14fa0dbaea47c518210c8542891
jlicini <licinijenny@gmail.com>
2021-11-24 10:35:33 +0100

    generators with recursion

--- generators.js
+++ generators.js
@@ -90,3 +90,37 @@ for (const name of [...testingTeam, ...engineeringTeam]) {
 }
 
 console.log(names);
+
+console.log("----")
+
+//generators with recursion
+
+class Comment{
+  constructor(content, children){
+    this.content = content;
+    this.children = children;
+  }
+
+  *[Symbol.iterator](){
+    yield this.content;
+    for (const child of this.children) {
+      yield* child
+    }
+  }
+}
+
+const children = [
+  new Comment('good comment', []),
+  new Comment('bad comment', []),
+  new Comment('meh', [])
+];
+
+const tree = new Comment('Great Post', children);
+
+const values = []
+
+for (const value of tree) {
+  values.push(value);
+}
+
+console.log(values)
