001017dcc169bceb5b76e9d86506a0165e8d5a1e
jlicini <licinijenny@gmail.com>
2021-11-24 10:16:19 +0100

    delegation of generators continued

--- generators.js
+++ generators.js
@@ -1,8 +1,8 @@
-const colors = ["red", "green", "blue"];
+const colorsArray = ["red", "green", "blue"];
 const numbersArray = [1, 2, 3, 4];
 let total = 0;
 
-for (const color of colors) {
+for (const color of colorsArray) {
   console.log(color);
 }
 
@@ -36,3 +36,62 @@ console.log(gen.next()); //leaving our house
 //purchase our stuff
 console.log(gen.next("groceries")); //leaving store with groceries
 console.log(gen.next("clean clothes"));
+
+console.log("--");
+
+function* colors() {
+    yield 'red';
+    yield 'blue';
+    yield 'green';
+}
+
+const color = colors();
+console.log(color.next());
+console.log(color.next());
+console.log(color.next());
+console.log(color.next());
+
+const myColors = []
+for (let color of colors()) {
+  myColors.push(color);
+}
+
+console.log("--")
+
+//practical use of generatos
+
+const testingTeam = {
+  lead: 'Amanda',
+  tester: 'Bill'
+};
+
+const engineeringTeam = {
+  size: 3,
+  department: 'Engineering',
+  lead: 'Jill',
+  manager: 'Alex',
+  engineering: 'Dave',
+  testingTeam
+};
+
+
+function* TeamIterator(team){
+  yield team.size;
+  yield team.manager;
+  yield team.engineering;
+  const TestingTeamGenerator = TestingTeamIterator(team.testingTeam);
+  yield* TestingTeamGenerator
+}
+
+function* TestingTeamIterator(team) {
+  yield team.lead;
+  yield team.tester;
+}
+
+const names = [];
+
+for (const name of TeamIterator(engineeringTeam)) {
+  names.push(name) //iterate in only particolar property
+}
+
+console.log(names);
